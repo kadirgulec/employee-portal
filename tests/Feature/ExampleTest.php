a736a41ae5dbff6d805964aca66536d2f5235348
Kadir <devguelec@gmail.com>
2024-08-28 15:06:40 +0200

    WIP Tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,15 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/login');
+use App\Models\Permission;
+use App\Models\User;
 
+it('returns a successful response', function () {
+    $user = User::factory()->create();
+    Permission::create(['name' => 'view-any User']);
+    $user->givePermissionTo('view-any User');
+    $this->actingAs($user);
+    $response = $this->get('/');
     $response->assertStatus(200);
 });
+
+
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+//    protected function setUp(): void
+//    {
+//        parent::setUp();
+//        $user = User::factory()->create();
+//        Permission::create(['name' => 'view-any User']);
+//        $user->givePermissionTo('view-any User');
+//        $this->actingAs($user);
+//    }
 }
